10c24aac3dcd6cb09312edff09ce942de5835d06
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-10-09 16:43:31 +0530

    Applied quick fixes.

---
+++ SingleCategorySuggest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\widgets\category;
+
+/**
+ * SingleCategorySuggest maps categories to models using auto-suggest.
+ *
+ * @since 1.0.0
+ */
+class SingleCategorySuggest extends CategorySuggest {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+	public $template = 'single-suggest';
+
+	public $column;
+	public $mapping;
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Widget --------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// SingleCategorySuggest -----------------
+
+}
